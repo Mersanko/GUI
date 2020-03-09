1de5b610817a0c9eab4c6fbcf429240360af34ad
zeamae <60580288+zeamae@users.noreply.github.com>
2020-03-09 22:37:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class template():
+  def __init__(self, fname, mname, lname, idnum, yrlvl, course):
+
+    self.fname = fname
+    self.mname = mname
+    self.lname = lname
+    self.idnum = idnum
+    self.yrlvl = yrlvl
+    self.course = course
